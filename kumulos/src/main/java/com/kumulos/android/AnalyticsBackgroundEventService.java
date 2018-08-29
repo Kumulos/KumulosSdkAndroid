26447414c45b1b2b645b428765e576a399e74678
Christopher Wyllie <c.wyllie@kumulos.com>
2018-08-29 16:05:15 +0100

    Don't include system logs in crashes reported from cold background process start

--- kumulos/src/main/java/com/kumulos/android/AnalyticsBackgroundEventService.java
+++ kumulos/src/main/java/com/kumulos/android/AnalyticsBackgroundEventService.java
@@ -1,10 +1,14 @@
 package com.kumulos.android;
 
+import android.app.Application;
 import android.os.Bundle;
 
 import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.TaskParams;
+
+import org.acra.ReportField;
+import org.acra.config.CoreConfigurationBuilder;
 
 public class AnalyticsBackgroundEventService extends GcmTaskService {
 
@@ -19,8 +23,21 @@ public class AnalyticsBackgroundEventService extends GcmTaskService {
         if (!Kumulos.isInitialized()) {
             Bundle configBundle = extras.getBundle(EXTRAS_KEY_CONFIG);
 
+            if (null == configBundle) {
+                return GcmNetworkManager.RESULT_FAILURE;
+            }
+
             KumulosConfig config = KumulosConfig.fromBundle(configBundle);
-            Kumulos.initialize(this.getApplication(), config);
+
+            Application application = getApplication();
+
+            if (config.crashReportingEnabled()) {
+                CoreConfigurationBuilder acraBuilder = config.getAcraConfigBuilder(application);
+                acraBuilder.setReportField(ReportField.USER_EMAIL, false);
+                acraBuilder.setReportField(ReportField.LOGCAT, false);
+            }
+
+            Kumulos.initialize(application, config);
         }
 
         long ts = extras.getLong(EXTRAS_KEY_TIMESTAMP);
