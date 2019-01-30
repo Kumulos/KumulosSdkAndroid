13273dee145ae360213ac8093aadb728e88ffd49
cgwyllie <cgwyllie@gmail.com>
2019-01-30 15:56:40 +0000

    Persist userIdentifier in shared preferences when associated (#11)
    * Persist userIdentifier in shared preferences when associated
    
    * Add accessor method for getting current locally associated userIdentifier
    
    * Move user association event out of critical region to prevent deadlocks
    
    In future we plan to record userIdentifier against tracked events, so we
    need to reduce the critical region to prevent deadlocking on writing the
    association event.
    
    * Fix typo

---
+++ kumulos/src/main/java/com/kumulos/android/SharedPrefs.java
@@ -0,0 +1,8 @@
+package com.kumulos.android;
+
+final class SharedPrefs {
+
+    static final String PREFS_FILE = "kumulos_prefs";
+    static final String KEY_USER_IDENTIFIER = "user_identifier";
+
+}
