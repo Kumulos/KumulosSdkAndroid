99a1815e8b585a575214a8172d39fcc00bab2603
Christopher Wyllie <c.wyllie@kumulos.com>
2019-01-31 10:36:55 +0000

    Insert current user identifier with events

--- kumulos/src/main/java/com/kumulos/android/AnalyticsContract.java
+++ kumulos/src/main/java/com/kumulos/android/AnalyticsContract.java
@@ -85,6 +85,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
             values.put(AnalyticsEvent.COL_EVENT_TYPE, this.eventType);
             values.put(AnalyticsEvent.COL_UUID, uuidStr);
             values.put(AnalyticsEvent.COL_HAPPENED_AT_MILLIS, this.happenedAt);
+            values.put(AnalyticsEvent.COL_USER_IDENTIFIER, Kumulos.getCurrentUserIdentifier(mContext));
 
             String propsStr = (null == this.properties) ? null : properties.toString();
             values.put(AnalyticsEvent.COL_PROPERTIES, propsStr);
