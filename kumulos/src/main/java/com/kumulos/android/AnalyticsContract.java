3266bc42465b01bf6d109a10e9529721b4f9eab5
Vladislavs Voicehovics <vladislav.voicehovich@gmail.com>
2019-08-30 11:09:43 +0100

    remove unused import

--- kumulos/src/main/java/com/kumulos/android/AnalyticsContract.java
+++ kumulos/src/main/java/com/kumulos/android/AnalyticsContract.java
@@ -14,7 +14,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
