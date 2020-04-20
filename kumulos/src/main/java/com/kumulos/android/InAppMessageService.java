70c814acc4665efaf9f9827d4e83e3916d60e3c9
Christopher Wyllie <c.wyllie@kumulos.com>
2020-04-20 12:00:55 +0100

    Move in-app read & present work to executor service

--- kumulos/src/main/java/com/kumulos/android/InAppMessageService.java
+++ kumulos/src/main/java/com/kumulos/android/InAppMessageService.java
@@ -3,6 +3,10 @@ package com.kumulos.android;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Pair;
 
 import org.json.JSONException;
@@ -16,6 +20,8 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import androidx.annotation.Nullable;
+
 class InAppMessageService {
     private static final String TAG = InAppMessageService.class.getName();
     private static final String PRESENTED_WHEN_IMMEDIATELY = "immediately";
@@ -23,18 +29,18 @@ class InAppMessageService {
     private static final String PRESENTED_WHEN_NEVER = "never";
     private static List<Integer> pendingTickleIds = new ArrayList<>();
 
-    static void clearAllMessages(Context context){
+    static void clearAllMessages(Context context) {
         Runnable task = new InAppContract.ClearDbRunnable(context);
         Kumulos.executorService.submit(task);
     }
 
-    static boolean fetch(Context context, boolean includeNextOpen){
+    static boolean fetch(Context context, boolean includeNextOpen) {
         SharedPreferences preferences = context.getSharedPreferences(SharedPrefs.PREFS_FILE, Context.MODE_PRIVATE);
         long millis = preferences.getLong(SharedPrefs.IN_APP_LAST_SYNC_TIME, 0L);
         Date lastSyncTime = millis == 0 ? null : new Date(millis);
 
         List<InAppMessage> inAppMessages = InAppRequestService.readInAppMessages(context, lastSyncTime);
-        if (inAppMessages == null){
+        if (inAppMessages == null) {
             return false;
         }
 
@@ -42,13 +48,13 @@ class InAppMessageService {
         return true;
     }
 
-    private static void showFetchedMessages(Context context, List<InAppMessage> inAppMessages, boolean includeNextOpen){
-        if (inAppMessages.isEmpty()){
+    private static void showFetchedMessages(Context context, List<InAppMessage> inAppMessages, boolean includeNextOpen) {
+        if (inAppMessages.isEmpty()) {
             return;
         }
 
-        for (InAppMessage message: inAppMessages){
-            if (message.getDismissedAt() != null || message.getInboxDeletedAt() != null){
+        for (InAppMessage message : inAppMessages) {
+            if (message.getDismissedAt() != null || message.getInboxDeletedAt() != null) {
                 clearNotification(context, message.getInAppId());
             }
         }
@@ -70,22 +76,22 @@ class InAppMessageService {
 
         trackDeliveredEvents(context, deliveredIds);
 
-        if (AnalyticsContract.ForegroundStateWatcher.isBackground()){
+        if (AnalyticsContract.ForegroundStateWatcher.isBackground()) {
             return;
         }
 
         List<InAppMessage> itemsToPresent = new ArrayList<>();
-        for(InAppMessage message: unreadMessages){
+        for (InAppMessage message : unreadMessages) {
             boolean hasPendingTickleId = false;
-            for(Integer pendingTickleId : pendingTickleIds){
-                if (message.getInAppId() == pendingTickleId){
+            for (Integer pendingTickleId : pendingTickleIds) {
+                if (message.getInAppId() == pendingTickleId) {
                     hasPendingTickleId = true;
                     break;
                 }
             }
             if (message.getPresentedWhen().equals(PRESENTED_WHEN_IMMEDIATELY)
                     || (includeNextOpen && message.getPresentedWhen().equals(PRESENTED_WHEN_NEXT_OPEN))
-                    || hasPendingTickleId){
+                    || hasPendingTickleId) {
                 itemsToPresent.add(message);
             }
         }
@@ -94,140 +100,99 @@ class InAppMessageService {
         pendingTickleIds.clear();
     }
 
-    private static void trackDeliveredEvents(Context context, List<Integer> deliveredIds ){
+    private static void trackDeliveredEvents(Context context, List<Integer> deliveredIds) {
 
         JSONObject params = new JSONObject();
 
-        for (Integer deliveredId: deliveredIds){
+        for (Integer deliveredId : deliveredIds) {
             try {
                 params.put("type", AnalyticsContract.MESSAGE_TYPE_IN_APP);
                 params.put("id", deliveredId);
 
                 Kumulos.trackEvent(context, AnalyticsContract.EVENT_TYPE_MESSAGE_DELIVERED, params);
-            }
-            catch (JSONException e) {
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    static void readMessages(Context context, boolean fromBackground, Integer tickleId){
-        Callable<List<InAppMessage>> task = new InAppContract.ReadInAppMessagesCallable(context);
-        final Future<List<InAppMessage>> future = Kumulos.executorService.submit(task);
-
-        List<InAppMessage> unreadMessages;
-        try {
-            unreadMessages = future.get();
-        } catch (InterruptedException | ExecutionException ex) {
-            return;
-        }
-
-        List<InAppMessage> itemsToPresent = new ArrayList<>();
-        for(InAppMessage message: unreadMessages){
-            if (message.getPresentedWhen().equals(PRESENTED_WHEN_IMMEDIATELY)
-                    || (fromBackground && message.getPresentedWhen().equals(PRESENTED_WHEN_NEXT_OPEN))
-                    || Integer.valueOf(message.getInAppId()).equals(tickleId)){
-                itemsToPresent.add(message);
-            }
-        }
-
-        List<Integer> tickleIds = new ArrayList<>();
-        if (tickleId != null){
-            boolean tickleMessageFound = false;
-            for (InAppMessage message : itemsToPresent){
-                if (message.getInAppId() == tickleId){
-                    tickleMessageFound = true;
-                    break;
-                }
-            }
-
-            if (!tickleMessageFound){
-                pendingTickleIds.add(tickleId);
-            }
-            else{
-                tickleIds.add(tickleId);
-            }
-        }
-
-        InAppMessagePresenter.presentMessages(itemsToPresent, tickleIds);
-
-        maybeDoExtraFetch(context, fromBackground);
+    static void readAndPresentMessages(Context context, boolean fromBackground, Integer tickleId) {
+        Runnable task = new ReadAndPresentMessagesRunnable(context, fromBackground, tickleId);
+        Kumulos.executorService.submit(task);
     }
 
-    private static void maybeDoExtraFetch(Context context, boolean fromBackground){
+    private static void maybeDoExtraFetch(Context context, boolean fromBackground) {
         boolean shouldFetch = false;
-        if (BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             shouldFetch = true;
-        }
-        else{
+        } else {
             SharedPreferences preferences = context.getSharedPreferences(SharedPrefs.PREFS_FILE, Context.MODE_PRIVATE);
             long lastSyncMillis = preferences.getLong(SharedPrefs.IN_APP_LAST_SYNC_TIME, 0L);
 
             Calendar now = Calendar.getInstance();
             now.setTime(new Date());
-            if (lastSyncMillis != 0L && lastSyncMillis +  3600 * 1000 < now.getTimeInMillis()){
+            if (lastSyncMillis != 0L && lastSyncMillis + 3600 * 1000 < now.getTimeInMillis()) {
                 shouldFetch = true;
             }
         }
 
-        if (shouldFetch){
-            new Thread(new Runnable() {
+        if (shouldFetch) {
+            Kumulos.executorService.submit(new Runnable() {
+                @Override
                 public void run() {
                     InAppMessageService.fetch(context, fromBackground);
                 }
-            }).start();
+            });
         }
     }
 
-    static void handleMessageClosed(Context context, InAppMessage message){
+    static void handleMessageClosed(Context context, InAppMessage message) {
         updateDismissedAt(context, message);
         trackDismissedEvent(context, message.getInAppId());
         clearNotification(context, message.getInAppId());
     }
 
-    private static void updateDismissedAt(Context context, InAppMessage message){
+    private static void updateDismissedAt(Context context, InAppMessage message) {
         message.setDismissedAt(new Date());
         Runnable task = new InAppContract.TrackMessageDismissedRunnable(context, message);
         Kumulos.executorService.submit(task);
     }
 
-    private static void trackDismissedEvent(Context context, int id){
+    private static void trackDismissedEvent(Context context, int id) {
         JSONObject params = new JSONObject();
         try {
             params.put("type", AnalyticsContract.MESSAGE_TYPE_IN_APP);
             params.put("id", id);
 
             Kumulos.trackEvent(context, AnalyticsContract.EVENT_TYPE_MESSAGE_DISMISSED, params);
-        }
-        catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    static void trackOpenedEvent(Context context, int id){
+    static void trackOpenedEvent(Context context, int id) {
         JSONObject params = new JSONObject();
         try {
             params.put("type", AnalyticsContract.MESSAGE_TYPE_IN_APP);
             params.put("id", id);
 
             Kumulos.trackEvent(context, AnalyticsContract.EVENT_TYPE_MESSAGE_OPENED, params);
-        }
-        catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    private static void clearNotification(Context context, int inAppId){
+    private static void clearNotification(Context context, int inAppId) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.cancel(PushBroadcastReceiver.KUMULOS_NOTIFICATION_TAG, inAppId);
     }
 
-    private static void storeLastSyncTime(Context context, List<InAppMessage> inAppMessages){
+    private static void storeLastSyncTime(Context context, List<InAppMessage> inAppMessages) {
         Date maxUpdatedAt = inAppMessages.get(0).getUpdatedAt();
 
-        for (int i=1; i<inAppMessages.size();i++){
+        for (int i = 1; i < inAppMessages.size(); i++) {
             Date messageUpdatedAt = inAppMessages.get(i).getUpdatedAt();
-            if (messageUpdatedAt.after(maxUpdatedAt)){
+            if (messageUpdatedAt.after(maxUpdatedAt)) {
                 maxUpdatedAt = messageUpdatedAt;
             }
         }
@@ -238,7 +203,7 @@ class InAppMessageService {
         editor.apply();
     }
 
-    static List<InAppInboxItem> readInboxItems(Context context){
+    static List<InAppInboxItem> readInboxItems(Context context) {
         Callable<List<InAppInboxItem>> task = new InAppContract.ReadInAppInboxCallable(context);
         final Future<List<InAppInboxItem>> future = Kumulos.executorService.submit(task);
 
@@ -253,7 +218,7 @@ class InAppMessageService {
         return inboxItems;
     }
 
-    static KumulosInApp.InboxMessagePresentationResult presentMessage(Context context, InAppInboxItem item){
+    static KumulosInApp.InboxMessagePresentationResult presentMessage(Context context, InAppInboxItem item) {
         Callable<InAppMessage> task = new InAppContract.ReadInAppInboxMessageCallable(context, item.getId());
         final Future<InAppMessage> future = Kumulos.executorService.submit(task);
 
@@ -264,11 +229,11 @@ class InAppMessageService {
             return KumulosInApp.InboxMessagePresentationResult.FAILED;
         }
 
-        if (inboxMessage == null){
-           return KumulosInApp.InboxMessagePresentationResult.FAILED;
+        if (inboxMessage == null) {
+            return KumulosInApp.InboxMessagePresentationResult.FAILED;
         }
 
-        if (item.getAvailableTo() != null && item.getAvailableTo().getTime() < new Date().getTime()){
+        if (item.getAvailableTo() != null && item.getAvailableTo().getTime() < new Date().getTime()) {
             return KumulosInApp.InboxMessagePresentationResult.FAILED_EXPIRED;
         }
 
@@ -280,15 +245,14 @@ class InAppMessageService {
         return KumulosInApp.InboxMessagePresentationResult.PRESENTED;
     }
 
-    static boolean deleteMessageFromInbox(Context context, int id){
+    static boolean deleteMessageFromInbox(Context context, int id) {
         JSONObject params = new JSONObject();
         try {
             params.put("type", AnalyticsContract.MESSAGE_TYPE_IN_APP);
             params.put("id", id);
 
             Kumulos.trackEvent(context, AnalyticsContract.MESSAGE_DELETED_FROM_INBOX, params);
-        }
-        catch (JSONException e) {
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
@@ -305,5 +269,95 @@ class InAppMessageService {
         }
 
         return result;
+    }
+
+    private static class ReadAndPresentMessagesRunnable implements Runnable {
+        private static final String TAG = ReadAndPresentMessagesRunnable.class.getName();
+
+        private Context mContext;
+        private boolean fromBackground;
+        private Integer tickleId;
+
+        ReadAndPresentMessagesRunnable(Context context, boolean fromBackground, @Nullable Integer tickleId) {
+            mContext = context.getApplicationContext();
+            this.fromBackground = fromBackground;
+            this.tickleId = tickleId;
+        }
+
+        @Override
+        public void run() {
+            List<InAppMessage> unreadMessages = getMessagesToPresent();
+
+            List<InAppMessage> itemsToPresent = new ArrayList<>();
+            for (InAppMessage message : unreadMessages) {
+                if (message.getPresentedWhen().equals(PRESENTED_WHEN_IMMEDIATELY)
+                        || (fromBackground && message.getPresentedWhen().equals(PRESENTED_WHEN_NEXT_OPEN))
+                        || Integer.valueOf(message.getInAppId()).equals(tickleId)) {
+                    itemsToPresent.add(message);
+                }
+            }
+
+            List<Integer> tickleIds = new ArrayList<>();
+            if (tickleId != null) {
+                boolean tickleMessageFound = false;
+                for (InAppMessage message : itemsToPresent) {
+                    if (message.getInAppId() == tickleId) {
+                        tickleMessageFound = true;
+                        break;
+                    }
+                }
+
+                if (!tickleMessageFound) {
+                    pendingTickleIds.add(tickleId);
+                } else {
+                    tickleIds.add(tickleId);
+                }
+            }
+
+            InAppMessagePresenter.presentMessages(itemsToPresent, tickleIds);
+
+            // TODO potential bug? logic in here doesn't take into account the pending tickles
+            //      in prod builds if synced < 1hr ago, may not sync again? (although assumed sync happens on app startup so...)
+            maybeDoExtraFetch(mContext, fromBackground);
+        }
+
+        private List<InAppMessage> getMessagesToPresent() {
+            SQLiteOpenHelper dbHelper = new InAppDbHelper(mContext);
+
+            List<InAppMessage> itemsToPresent = new ArrayList<>();
+            try {
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+                String[] projection = {InAppContract.InAppMessageTable.COL_ID, InAppContract.InAppMessageTable.COL_PRESENTED_WHEN, InAppContract.InAppMessageTable.COL_CONTENT_JSON};
+                String selection = String.format("%s IS NULL AND (%s IS NULL OR (DATETIME(%s) > DATETIME('now')))",
+                        InAppContract.InAppMessageTable.COL_DISMISSED_AT,
+                        InAppContract.InAppMessageTable.COL_EXPIRES_AT,
+                        InAppContract.InAppMessageTable.COL_EXPIRES_AT);
+
+                String sortOrder = InAppContract.InAppMessageTable.COL_UPDATED_AT + " ASC";
+
+                Cursor cursor = db.query(InAppContract.InAppMessageTable.TABLE_NAME, projection, selection, null, null, null, sortOrder);
+
+                while (cursor.moveToNext()) {
+                    int inAppId = cursor.getInt(cursor.getColumnIndexOrThrow(InAppContract.InAppMessageTable.COL_ID));
+                    String content = cursor.getString(cursor.getColumnIndexOrThrow(InAppContract.InAppMessageTable.COL_CONTENT_JSON));
+                    String presentedWhen = cursor.getString(cursor.getColumnIndexOrThrow(InAppContract.InAppMessageTable.COL_PRESENTED_WHEN));
+                    InAppMessage m = new InAppMessage();
+                    m.setInAppId(inAppId);
+                    m.setContent(new JSONObject(content));
+                    m.setPresentedWhen(presentedWhen);
+                    itemsToPresent.add(m);
+                }
+                cursor.close();
+
+                dbHelper.close();
+            } catch (SQLiteException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                Kumulos.log(TAG, e.getMessage());
+            }
+
+            return itemsToPresent;
+        }
     }
 }
